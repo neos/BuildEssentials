2b9912c6bfbd28aedff33dca943c75e1f63d6e17
Karsten Dambekalns <karsten@typo3.org>
2013-08-22 15:45:53 +0200

    [TASK] Tweak ReleaseHelpers.sh
    Change-Id: Ia7a486ca091296daf4ee12ee25053a1e26578d99
    Releases: master, 2.0

--- ReleaseHelpers.sh
+++ ReleaseHelpers.sh
@@ -1,33 +1,59 @@
 #!/bin/bash
 
-# arguments: VERSION, BRANCH, BUILD_URL
+# arguments: VERSION, BRANCH, BUILD_URL, DIR (optional)
 function tag_version {
-	local $*
+	local VERSION=$1
+	local BRANCH=$2
+	local BUILD_URL=$3
+	local DIR=$4
 
-	git tag -f -a -m "[TASK] Tag as version ${VERSION}" -m "See ${BUILD_URL}" -m "Releases: ${BRANCH}" ${VERSION}
+	if [ -z "${DIR}" ] ; then
+		git tag -f -a -m "[TASK] Tag as version ${VERSION}" -m "See ${BUILD_URL}" -m "Releases: ${BRANCH}" ${VERSION}
+	else
+		git --git-dir "${DIR}/.git" tag -f -a -m "[TASK] Tag as version ${VERSION}" -m "See ${BUILD_URL}" -m "Releases: ${BRANCH}" ${VERSION}
+	fi
 }
 
-# arguments: TAG
+# arguments: TAG, DIR (optional)
 function push_tag {
-	local $*
+	local TAG=$1
+	local DIR=$2
 
-	git push -f origin ${TAG}:refs/tags/${TAG}
+	if [ -z "${DIR}" ] ; then
+		git push -f origin ${TAG}:refs/tags/${TAG}
+	else
+		git --git-dir "${DIR}/.git" push -f origin ${TAG}:refs/tags/${TAG}
+	fi
 }
 
-# arguments: BRANCH, BUILD_URL
+# arguments: BRANCH, BUILD_URL, DIR (optional)
 function commit_manifest_update {
-	local $*
+	local BRANCH=$1
+	local BUILD_URL=$2
+	local DIR=$3
 
-	if [ "`git status --porcelain composer.json`" ] ; then
-		git add composer.json
-		git commit -m "[TASK] Update composer manifest" -m "See $BUILD_URL" -m "Releases: ${BRANCH}"
+	if [ -z "${DIR}" ] ; then
+		if [[ `git status --porcelain composer.json` ]] ; then
+			git add composer.json
+			git commit -m "[TASK] Update composer manifest" -m "See ${BUILD_URL}" -m "Releases: ${BRANCH}"
+		fi
+	else
+		if [[ `git --git-dir "${DIR}/.git" --work-tree "${DIR}" status --porcelain composer.json` ]] ; then
+			git --git-dir "${DIR}/.git" --work-tree "${DIR}" add composer.json
+			git --git-dir "${DIR}/.git" --work-tree "${DIR}" commit -m "[TASK] Update composer manifest" -m "See ${BUILD_URL}" -m "Releases: ${BRANCH}"
+		fi
 	fi
 
 }
 
-# arguments: BRANCH
+# arguments: BRANCH, DIR (optional)
 function push_branch {
-	local $*
+	local BRANCH=$1
+	local DIR=$2
 
-	git push origin ${BRANCH}
+	if [ -z "${DIR}" ] ; then
+		git push origin ${BRANCH}
+	else
+		git --git-dir "${DIR}/.git" push origin ${BRANCH}
+	fi
 }
