e49d99d1cabb4209ca16b4e5fc46595d4c4182fb
Karsten Dambekalns <karsten@typo3.org>
2014-03-25 13:25:36 +0100

    [FEATURE] Helpers for crowdin translation tool usage
    Adds some helpers for integration with the crowdin translation
    tool.
    
    Change-Id: Ica87bfd21dbdf1d68965e228e1a17754f0e8608b
    Releases: master, 2.2, 2.1

---
+++ Crowdin/Upload.php
@@ -0,0 +1,25 @@
+<?php
+
+require(__DIR__ . '/Helpers.php');
+
+foreach ($projects as $identifier => $projectData) {
+	$projectInfo = json_decode(file_get_contents(sprintf('http://api.crowdin.net/api/project/%s/info?json&key=%s', $identifier, $projectData['apiKey'])), TRUE);
+
+	// check if project exists
+	if (isset($projectInfo['success']) && $projectInfo['success'] === FALSE) {
+		echo 'Project ' . $projectData['name'] . ' does not exist, please create it.' . PHP_EOL;
+		continue;
+	}
+
+	$projectPath = realpath(__DIR__ . '/../../../' . $projectData['path']);
+	if (!file_exists($projectPath . '/crowdin.yaml')) {
+		echo 'Project ' . $projectData['name'] . ' is not configured, skipping.' . PHP_EOL;
+		continue;
+	}
+
+	echo 'Working on ' . $projectPath . PHP_EOL;
+	passthru(sprintf('cd %s; crowdin-cli upload sources', $projectPath));
+	if (isset($argv[2]) && $argv[2] === '--translations') {
+		passthru(sprintf('cd %s; crowdin-cli upload translations', $projectPath));
+	}
+}
