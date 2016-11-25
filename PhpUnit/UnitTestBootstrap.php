345c9ea13de34776740cae4134612ffc8b4b2a2c
Christian Mueller <christian@flownative.com>
2016-11-25 07:34:16 +0100

    TASK: Adapt buildessentials to new namespaces

--- PhpUnit/UnitTestBootstrap.php
+++ PhpUnit/UnitTestBootstrap.php
@@ -1,24 +1,24 @@
 <?php
 namespace Neos\Flow\Build;
 
-/*                                                                        *
- * This script belongs to the TYPO3 Flow build system.                    *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License, either version 3 of the   *
- * License, or (at your option) any later version.                        *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/*
+ * This file is part of the Neos Flow build system.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
 $composerAutoloader = __DIR__ . '/../../../Packages/Libraries/autoload.php';
 if(!file_exists($composerAutoloader)) {
-	exit(PHP_EOL . 'TYPO3 Flow Bootstrap Error: The unit test bootstrap requires the autoloader file created at install time by Composer. Looked for "' . $composerAutoloader . '" without success.');
+	exit(PHP_EOL . 'Neos Flow Bootstrap Error: The unit test bootstrap requires the autoloader file created at install time by Composer. Looked for "' . $composerAutoloader . '" without success.');
 }
 require_once($composerAutoloader);
 
 if (!class_exists('org\bovigo\vfs\vfsStream')) {
-	exit(PHP_EOL . 'TYPO3 Flow Bootstrap Error: The unit test bootstrap requires vfsStream to be installed. Try "composer update --dev".' . PHP_EOL . PHP_EOL);
+	exit(PHP_EOL . 'Neos Flow Bootstrap Error: The unit test bootstrap requires vfsStream to be installed. Try "composer update --dev".' . PHP_EOL . PHP_EOL);
 }
 
 spl_autoload_register('Neos\Flow\Build\loadClassForTesting');
--- TravisCi/SetupDatabase.sh
+++ TravisCi/SetupDatabase.sh
@@ -4,7 +4,7 @@ if [ "$DB" = "pgsql" ]; then
 	psql -c 'DROP DATABASE IF EXISTS flow_functional_testing;' -U postgres
 	psql -c 'CREATE DATABASE flow_functional_testing;' -U postgres
 	cat <<EOF > Configuration/Settings.yaml
-TYPO3:
+Neos:
   Flow:
     persistence:
       backendOptions:
@@ -17,7 +17,7 @@ fi
 if [ "$DB" = "mysql" ]; then
 	mysql -e 'CREATE DATABASE IF NOT EXISTS flow_functional_testing;'
 	cat <<EOF > Configuration/Settings.yaml
-TYPO3:
+Neos:
   Flow:
     persistence:
       backendOptions:
