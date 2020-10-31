a7cb87937a31c9652496db4a5af88bdf28a664a3
Alexander Berl <a.berl@outlook.com>
2020-10-31 14:59:24 +0100

    TASK: Fix merging require-dev section when it doesn't exist in target

--- TravisCi/ComposerManifestUpdater.php
+++ TravisCi/ComposerManifestUpdater.php
@@ -44,7 +44,7 @@ if(isset($composerManifest['require'][$targetRepository])) {
 // Merge dev requirements from dev-collection
 $buildComposerManifest = json_decode(file_get_contents('../../' . $travisRepoSlug . '/composer.json'), true);
 if (isset($buildComposerManifest['require-dev'])) {
-    $composerManifest['require-dev'] = array_merge($composerManifest['require-dev'], $buildComposerManifest['require-dev']);
+    $composerManifest['require-dev'] = array_merge($composerManifest['require-dev'] ?? [], $buildComposerManifest['require-dev']);
 }
 
 $output = json_encode($composerManifest, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
