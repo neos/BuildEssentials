d1967ef72ac2605d0e0cb59c177c89b3c3467b14
Karsten Dambekalns <karsten@dambekalns.de>
2022-06-23 13:39:33 +0200

    TASK: Drop outdated Crowdin integration code

--- Crowdin/Download.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require(__DIR__ . '/Helpers.php');
-
-$projectPath = realpath(__DIR__ . '/../../../');
-echo 'Downloading…' . PHP_EOL;
-executeDownload($projectPath, $branch);
--- Crowdin/Upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require(__DIR__ . '/Helpers.php');
-
-$projectPath = realpath(__DIR__ . '/../../../');
-echo 'Uploading…' . PHP_EOL;
-executeUpload($projectPath, $uploadTranslations, $branch);
